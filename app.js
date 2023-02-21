9a1f61ccf8f1fd4474f5df58f8da72e2b27f3f95
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-21 18:02:16 +0100

    modified

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const circle = document.querySelector('#circle');
-const upBtn = document.querySelector('#upBtn');
-const downBtn = document.querySelector('#downBtn');
+const circle = document.getElementById('circle');
+const upBtn = document.getElementById('upBtn');
+const downBtn = document.getElementById('downBtn');
+
+let rotation = 0
+function rotateUp() {
+	rotation += 90;
+	if (rotation === 360) {
+		rotation = 0
+	}
+
+	circle.style.transform = `rotate(${rotation}deg)`
+};
+
+function rotateDown() {
+	rotation += -90;
+	if (rotation === 360) {
+		rotation = 0
+	}
+
+	circle.style.transform = `rotate(${rotation}deg)`
+};
+
+upBtn.addEventListener('click', rotateUp)
+downBtn.addEventListener('click', rotateDown)
